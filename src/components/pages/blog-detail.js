3e4a358ecd4e593d4e85835fbdbccded44b31988
Jake <gosnelljacob@gmail.com>
2019-08-01 21:08:22 -0700

    rendering the blog details to the screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-export default class extends Component {
+export default class BlogDetail extends Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem () {
+    axios.get(`https://jake.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+    ).then(response => {
+      console.log("response", response);
+      this.setState({
+        blogItem: response.data.portfolio_blog
+      });
+    }).catch(error => {
+      console.log("getBlogItemError", error);
+    });
+  }
+
+  componentDidMount () {
+    this.getBlogItem();
   }
 
   render () {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
       <div>
-        <h1>Blog Detail Component</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} />
+        <div>{content}</div>
       </div>
     );
   }
