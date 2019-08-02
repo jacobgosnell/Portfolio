a6d5299f8de5667bf0384344646f4f720f3b6d17
Jake <gosnelljacob@gmail.com>
2019-08-01 19:34:15 -0700

    built initial blog detail component, and wired up new link from the title to the new blog-detail page

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BlogItem = props => {
   const {
@@ -11,10 +12,10 @@ const BlogItem = props => {
 
   return (
     <div>
-    <h1>{title}</h1>
-    <div>
-    {content}
-    </div>
+    <Link to={`/b/${id}`}>
+      <h1>{title}</h1>
+    </Link>
+    <div>{content}</div>
     </div>
   );
 };
---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Blog Detail Component</h1>
+      </div>
+    );
+  }
+}
