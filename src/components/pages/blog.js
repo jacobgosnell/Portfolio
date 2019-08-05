3ef4d18af05f5a65532b0c17f809cf9e425b748b
Jake <gosnelljacob@gmail.com>
2019-08-04 22:30:54 -0700

    migrated event listener from in function to constructor, added listener using different syntax, and removed the event listener in lifecycle hook componentWillUnmount

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -16,23 +16,22 @@ class Blog extends Component {
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
-  activateInfiniteScroll () {
-    window.onscroll = () => {
-      if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
-        return;
-      }
+  onScroll () {
+    if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+      return;
+    }
 
-      // console.log("window.innerHeight", window.innerHeight);
-      // console.log("document.documentElement.scrollTop", document.documentElement.scrollTop);
-      // console.log("document.documentElement.offsetHeight", document.documentElement.offsetHeight);
+    // console.log("window.innerHeight", window.innerHeight);
+    // console.log("document.documentElement.scrollTop", document.documentElement.scrollTop);
+    // console.log("document.documentElement.offsetHeight", document.documentElement.offsetHeight);
 
-      if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-        this.getBlogItems();
-      }
-    };
+    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems () {
@@ -55,6 +54,10 @@ class Blog extends Component {
 
   componentWillMount () {
     this.getBlogItems();
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render () {
