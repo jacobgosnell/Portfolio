96f73d98ea79f4ba86e79e9f953d822f08d5c12c
Jake <gosnelljacob@gmail.com>
2019-08-11 17:30:22 -0700

    consolidated icon imports to a new component category helper file

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from 'axios';
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -14,12 +11,13 @@ import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
-
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+import Icons from "../helpers/icons";
 
 export default class App extends Component {
   constructor (props) {
     super(props);
+
+    Icons(); 
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
@@ -112,7 +110,11 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              <Route path="/blog"
+                render={props => (
+                  <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+                )}
+              />
               <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedRoutes() : null }
               <Route
