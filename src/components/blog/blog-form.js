84d2730e10acc36ca7dc32dfe35377676e07806e
Jake <gosnelljacob@gmail.com>
2019-08-12 20:38:15 -0700

    Fixing Set State on an Unmounted Component Warning in React

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -35,11 +35,13 @@ export default class BlogForm extends Component {
   handleSubmit (event) {
     axios.post("https://jake.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), { withCredentials: true})
     .then(response => {
-      this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
       this.setState({
         title: "",
-        blog_status: ""
+        blog_status: "",
+        content: ""
       });
+      this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
+      
     }).catch(error => {
       console.log("handleSubmit for blog error", error);
     });
