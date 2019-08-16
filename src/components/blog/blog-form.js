8c95ff1b0cc9450c756bdf8e6e13b8ceed0cbcb3
Jake <gosnelljacob@gmail.com>
2019-08-15 19:48:31 -0700

    configuring dropzone for blog featured image

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
+
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -10,12 +12,38 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
-      content: ""
+      content: "",
+      featured_image: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post"
+    }
+  }
+
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1
+    }
+  }
+
+  handleFeaturedImageDrop() {
+    return {
+      addedfile: file => this.setState({ featured_image: file })
+    }
   }
 
   handleRichTextEditorChange (content) {
@@ -77,6 +105,16 @@ export default class BlogForm extends Component {
 
         <div className="one-column">
           <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange} />
+        </div>
+
+        <div className="image-uploaders">
+          <DropzoneComponent
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleFeaturedImageDrop()}
+          >
+            <div className="dz-message">Featured Image</div>
+          </DropzoneComponent>
         </div>
         <button className="btn">Save</button>
       </form>
