0f63f03ff28a1138f4cedf8598d9d275c19e493c
Jake <gosnelljacob@gmail.com>
2019-08-19 20:16:35 -0700

    rendering the portfolio detail data on the screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,7 +4,9 @@ import axios from "axios";
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      portfolioItem: {}
+    };
 
     this.getOnePortfolioItem = this.getOnePortfolioItem.bind(this);
   }
@@ -12,20 +14,35 @@ export default class PortfolioDetail extends Component {
   componentWillMount () {
     this.getOnePortfolioItem();
   }
-  
+
   getOnePortfolioItem () {
     axios.get(`https://jake.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`
     ).then(response => {
       console.log("response", response);
+      this.setState({
+        portfolioItem: response.data.portfolio_item
+      })
+
     }).catch(error => {
       console.log("getPortfolioItem Error", error);
     });
   }
 
   render() {
+    const {
+      banner_image_url,
+      category, 
+      description, 
+      logo_url, 
+      name,
+      thumb_image_url, 
+      url
+    } = this.state.portfolioItem;
+
     return (
       <div>
-        <h2 onClick={this.getOnePortfolioItem}>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     )
   }
