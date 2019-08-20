44fc0f1358af85eed406f9f292411646a226b566
Jake <gosnelljacob@gmail.com>
2019-08-19 21:00:51 -0700

    fun filtering feature built out on homepage (needs fixing)

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -16,24 +16,30 @@ export default class PortfolioContainer extends Component {
     this.handleFilter = this.handleFilter.bind(this);
   }
 
-  // list out any custom functions you'd want to use
-
   handleFilter (filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.category === filter;
-      })
-    });
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.setState({
+        data: this.state.data
+      });
+    }
   }
 
-  getPortfolioItems () {
+  getPortfolioItems (filter = null) {
     axios
     .get('https://jake.devcamp.space/portfolio/portfolio_items')
     .then(response => {
-    console.log('response data', response);
-      this.setState({
-        data: response.data.portfolio_items
-      });
+      if (filter) {
+
+        } else {
+        console.log('response data', response);
+        this.setState({
+          data: response.data.portfolio_itemsfilter(item => {
+            return item.category === filter;
+          })
+        });
+      }
     })
     .catch(error => {
       console.log(error);
@@ -62,11 +68,18 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-        <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-        <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-        {this.portfolioItems()}
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+          <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+          <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+          <button className="btn" onClick={() => this.handleFilter('CLEAR_FILTERS')}>All</button>
+        </div>
+
+        <div className="portfolio-items-wrapper">
+          
+          {this.portfolioItems()}
+        </div>
       </div>
     );
   }
